b27c7780bbb8fa257474bc4d94f56af9648b7217
Daniel Couzens <dan.couzens@epimorphics.com>
2024-09-14 07:31:22 +0100

    adds d-mojor and creates list endpoint

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const express = require("express")
+const express = require("express");
 const cors = require('cors');
-const fs = require("fs")
-const app = express()
-const port = 8080
+const fs = require("fs");
+const app = express();
+const port = 8080;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Chord Library API!');
-  });
+});
 
-app.get("/chord-library/c-major", (req, res) => {
-  fs.readFile("./data/chord-library/c-major.json", "utf8", (err, data) => {
-    if (err) {
-      res.status(500).send("Error reading chords file")
-    } else {
-      res.send(JSON.parse(data))
-    }
-  })
-})
+app.get("/chord-library/:chord", (req, res) => {
+    const chord = req.params.chord;
+    const filePath = `./data/chord-library/${chord}.json`;
+    
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.status(404).send("Chord file not found");
+            } else {
+                res.status(500).send("Error reading chords file");
+            }
+        } else {
+            res.send(JSON.parse(data));
+        }
+    });
+});
+
+app.get("/chord-library", (req, res) => {
+  fs.readdir("./data/chord-library", (err, files) => {
+      if (err) {
+          res.status(500).send("Error reading chord library directory");
+      } else {
+          const chords = files
+              .filter(file => file.endsWith('.json'))
+              .map(file => file.replace('.json', ''));
+          res.send(chords);
+      }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
